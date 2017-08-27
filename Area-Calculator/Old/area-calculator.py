af5b1a6ec90b661a33d691b23f8f5db4d7a6b35b
root <root@localhost.localdomain>
2017-08-27 11:23:20 -0400

    update to area calculator

--- Area-Calculator/Old/area-calculator.py
+++ Area-Calculator/Old/area-calculator.py
@@ -2,6 +2,8 @@ import random
 import time
 import turtle
 def turtledraw(lenght, hight):
+    my_turtle = turtle.Turtle()
+    my_turtle.speed(5)
     for i in range(1000):
         my_turtle.forward(lenght)
         my_turtle.left(hight)
@@ -10,6 +12,4 @@ print("example lenght=100 left=90")
 
 data=input("Leght: ")
 data1=input("hight: ")
-my_turtle = turtle.Turtle()
-my_turtle.speed(5)
 turtledraw(int(data),int(data1))
