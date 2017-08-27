fe400e3eccad103d8f51f640936050ce739a7f2b
root <root@localhost.localdomain>
2017-08-27 17:45:19 -0400

    six-new-colours-added

--- Area-Calculator/area-calculator.py
+++ Area-Calculator/area-calculator.py
@@ -1,10 +1,18 @@
 import random
 import time
 import turtle
+color={
+1:"red", 2:"green", 3:"blue", 4:"pink", 5:"orange", 6:"black"
+    }
+randcolor=(random.randint(1,6))
 def turtledraw(lenght, hight):
-    for i in range(1000):
-        my_turtle.forward(lenght)
-        my_turtle.left(hight)
+    try:
+        for i in range(1000):
+            my_turtle.color(color[randcolor])
+            my_turtle.forward(lenght)
+            my_turtle.left(hight)
+    except KeyboardInterrupt:
+        print("Exiting")
 
 print("example lenght=100 left=90")
 
