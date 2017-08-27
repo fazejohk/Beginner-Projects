59a9ac1f597cd7c2479d221fcfc0ea180aef7bb9
root <root@localhost.localdomain>
2017-08-27 00:53:48 -0400

    added sentence-gen

---
+++ Sentence-Gen/sentencegen.py
@@ -0,0 +1,31 @@
+import random
+import time
+first={
+1:"What", 2:"Where", 3:"When", 4:"who"
+    }
+
+second={
+1:"did", 2:"you", 3:"were", 4:"do"
+    }
+third={
+1:"it", 2:"she", 3:"he", 4:"nibba"
+    }
+fourth={
+1:"do", 2:"eat", 3:"shit", 4:"steal"
+    }
+random1=random.randint(1,4)
+random2=random.randint(1,4)
+print(first[random1])
+time.sleep(1)
+print(second[random2])
+time.sleep(1)
+print(third[random1])
+time.sleep(1)
+print(fourth[random2])
+time.sleep(1)
+print("""
+
+
+
+
+ """)
