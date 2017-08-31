8181e865e43ea22803bc36706401e0baee61004c
root <root@localhost.localdomain>
2017-08-31 21:01:24 +0300

    colour-system

--- Area-Calculator/area-calculator.py
+++ Area-Calculator/area-calculator.py
@@ -1,20 +1,18 @@
-import random
 import time
 import turtle
-color={
-1:"red", 2:"green", 3:"blue", 4:"pink", 5:"orange", 6:"black"
-    }
-randcolor=(random.randint(1,6))
+print("example lenght=100 left=90")
+print("Ctrl + C to Exit")
+colori=str(input("Select your color first letter must start whit a capital letter: "))
 def turtledraw(lenght, hight):
     try:
         for i in range(1000):
-            my_turtle.color(color[randcolor])
+            my_turtle.color(colori)
             my_turtle.forward(lenght)
             my_turtle.left(hight)
     except KeyboardInterrupt:
         print("Exiting")
 
-print("example lenght=100 left=90")
+
 
 data=input("Leght: ")
 data1=input("hight: ")
