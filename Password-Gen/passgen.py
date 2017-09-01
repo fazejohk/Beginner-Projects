779b809193acdbcc34e5043435d5a692f237f90d
root <root@localhost.localdomain>
2017-09-01 22:39:25 +0300

    password-gen

---
+++ Password-Gen/passgen.py
@@ -0,0 +1,57 @@
+import random
+
+passwords={
+1:"S", 2:"q", 3:"o", 4:"P", 5:"5",
+6:"b", 7:"A", 8:"W", 9:"g", 10:"010",
+11:"B", 12:"L", 13:"f", 14:"y", 15:"313",
+16:"z", 17:"m", 18:"N", 19:"å", 20:"010",
+21:"ö", 22:"ä", 23:"R", 24:"B", 25:"10",
+26:"Y", 27:"h", 28:"k", 29:"e", 30:"201",
+31:"c", 32:"l", 33:"u", 34:"o", 35:"13",
+36:"v", 37:"x", 38:"t", 39:"Å", 40:"12"
+
+}
+
+r=int(random.randint(1,40))
+r1=int(random.randint(1,40))
+r2=int(random.randint(1,40))
+r3=int(random.randint(1,40))
+r4=int(random.randint(1,40))
+r5=int(random.randint(1,40))
+r6=int(random.randint(1,40))
+r7=int(random.randint(1,40))
+r8=int(random.randint(1,40))
+r9=int(random.randint(1,40))
+r10=int(random.randint(1,40))
+r11=int(random.randint(1,40))
+r12=int(random.randint(1,40))
+r13=int(random.randint(1,40))
+r14=int(random.randint(1,40))
+
+rs=(passwords[r])
+rs1=(passwords[r1])
+rs2=(passwords[r2])
+rs3=(passwords[r3])
+rs4=(passwords[r4])
+rs5=(passwords[r5])
+rs6=(passwords[r6])
+rs7=(passwords[r7])
+rs8=(passwords[r8])
+rs9=(passwords[r9])
+rs10=(passwords[r10])
+rs11=(passwords[r11])
+rs12=(passwords[r12])
+rs13=(passwords[r13])
+rs14=(passwords[r14])
+
+data=int(input("How many letters and numbers 5, 10 or 15: "))
+
+if data==5:
+    print(rs + rs1 + rs2 + rs3 + rs4)
+elif data==10:
+    print(rs + rs1 + rs2 + rs3 + rs4 + rs5 + rs6 + rs7 + rs8 + rs9)
+elif data==15:
+    print(rs + rs1 + rs2 + rs3 + rs4 + rs5 + rs6 + rs7 + rs8 + rs9 + rs10 + rs11 + rs12 + rs13 + rs14)
+else:
+    print("5, 10 or 15")
+    pass
